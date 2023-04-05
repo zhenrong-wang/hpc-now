e3becc5c96026b58f3f18b7144cda9263eacec6a
Zhenrong-WANG <zhenrongwang@live.com>
2023-04-05 13:09:20 +0000

    new
    Signed-off-by: Zhenrong-WANG <zhenrongwang@live.com>

--- installer/installer-darwin.c
+++ installer/installer-darwin.c
@@ -319,17 +319,17 @@ int update_services(void){
 int main(int argc, char* argv[]){
     int run_flag=0;
     print_header();
-    run_flag=license_confirmation();
-    
-    if(run_flag!=0){
-        print_tail();
-        return run_flag;
-    }
 
     if(argc!=2){
         print_help();
         print_tail();
         return 1;
+    }
+
+    run_flag=license_confirmation();
+    if(run_flag!=0){
+        print_tail();
+        return run_flag;
     }
     
     if(strcmp(argv[1],"uninstall")!=0&&strcmp(argv[1],"update")!=0&&strcmp(argv[1],"install")!=0){
