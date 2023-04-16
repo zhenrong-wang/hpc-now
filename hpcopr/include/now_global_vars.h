ca09fb4d312bc5f0d17fadf3825b4519eaf1ae6b
zhenrong-wang <zhenrongwang@live.com>
2023-04-16 13:53:49 +0000

    headers
    Signed-off-by: zhenrong-wang <zhenrongwang@live.com>

---
+++ hpcopr/include/now_global_vars.h
@@ -0,0 +1,23 @@
+/*
+* This code is written and maintained by Zhenrong WANG (mailto: wangzhenrong@hpc-now.com) 
+* The founder of Shanghai HPC-NOW Technologies Co., Ltd (website: https://www.hpc-now.com)
+* It is distributed under the license: GNU Public License - v2.0
+* Bug report: info@hpc-now.com
+*/
+
+#ifndef NOW_GLOBAL_VARS_H
+#define NOW_GLOBAL_VARS_H
+
+extern char URL_REPO_ROOT[LOCATION_LENGTH];
+extern char URL_ALICLOUD_ROOT[LOCATION_LENGTH];
+extern char URL_AWS_ROOT[LOCATION_LENGTH];
+extern char URL_QCLOUD_ROOT[LOCATION_LENGTH];
+extern char URL_SHELL_SCRIPTS[LOCATION_LENGTH];
+extern char URL_NOW_CRYPTO[LOCATION_LENGTH];
+extern int REPO_LOC_FLAG;
+extern int TEMPLATE_LOC_FLAG_ALI;
+extern int TEMPLATE_LOC_FLAG_AWS;
+extern int TEMPLATE_LOC_FLAG_QCLOUD;
+extern int NOW_CRYPTO_LOC_FLAG;
+
+#endif
