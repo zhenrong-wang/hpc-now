4d5906774120306cc9acf28e3838a1ef7822aa46
Zhenrong WANG <wangzhenrong@hpc-now.com>
2023-04-19 21:33:09 +0800

    changed the ls-clusters

--- hpcopr/src/cluster_operations.c
+++ hpcopr/src/cluster_operations.c
@@ -124,16 +124,27 @@ int delete_from_cluster_registry(char* deleted_cluster_name){
 int list_all_cluster_names(void){
     FILE* file_p=fopen(ALL_CLUSTER_REGISTRY,"r");
     char registry_line[LINE_LENGTH_SHORT]="";
+    char temp_cluster_name[CLUSTER_ID_LENGTH_MAX_PLUS]="";
+    int readline_flag=0;
     if(file_p==NULL){
         printf("[ FATAL: ] Cannot open the registry. the HPC-NOW service cannot work properly. Exit now.\n");
         return -1;
     }
     printf("[ -INFO- ] Current cluster list:\n");
-    while(fgetline(file_p,registry_line)==0){
+    do{
+        readline_flag=fgetline(file_p,registry_line);
         if(strlen(registry_line)!=0){
-            printf("           %s\n",registry_line);
+            if(file_exist_or_not(CURRENT_CLUSTER_INDICATOR)!=0){
+                printf("|          %s\n",registry_line);
+            }
+            else{
+                get_seq_string(registry_line,' ',3,temp_cluster_name);
+                if(find_multi_keys(CURRENT_CLUSTER_INDICATOR,temp_cluster_name,"","","","")>0){
+                    printf("| CUR ---> %s\n",registry_line);
+                }
+            }
         }
-    }
+    }while(readline==0);
     return 0;
 }
 
