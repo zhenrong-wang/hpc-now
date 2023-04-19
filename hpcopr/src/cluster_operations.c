8f84bcd03c2f9864f6263c6b8d1793f637470833
Zhenrong WANG <wangzhenrong@hpc-now.com>
2023-04-19 23:14:50 +0800

    changed

--- hpcopr/src/cluster_operations.c
+++ hpcopr/src/cluster_operations.c
@@ -140,19 +140,19 @@ int list_all_cluster_names(void){
         printf("[ FATAL: ] Cannot open the registry. the HPC-NOW service cannot work properly. Exit now.\n");
         return -1;
     }
-    printf("[ -INFO- ] Cluster list:\n");
+    printf("[ -INFO- ] Cluster list:\n\n");
     while(fgetline(file_p,registry_line)!=1){
         if(strlen(registry_line)!=0){
             if(file_exist_or_not(CURRENT_CLUSTER_INDICATOR)!=0){
-                printf("|          %s\n",registry_line);
+                printf("           %s\n",registry_line);
             }
             else{
                 get_seq_string(registry_line,' ',3,temp_cluster_name);
                 if(find_multi_keys(CURRENT_CLUSTER_INDICATOR,temp_cluster_name,"","","","")>0){
-                    printf("| CUR ---> %s\n",registry_line);
+                    printf("< ACTIVE > %s\n",registry_line);
                 }
                 else{
-                    printf("|          %s\n",registry_line);
+                    printf("           %s\n",registry_line);
                 }
             }
         }
