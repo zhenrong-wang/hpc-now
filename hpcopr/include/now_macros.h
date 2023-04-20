6a66001dd2e05be9fc8d869dc8c614234f9275f2
Zhenrong WANG <wangzhenrong@hpc-now.com>
2023-04-20 19:14:12 +0800

    updated the version code

--- hpcopr/include/now_macros.h
+++ hpcopr/include/now_macros.h
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include <signal.h>
 
-#define VERSION_CODE "0.2.1.0003"
+#define VERSION_CODE "0.2.1.0005"
 
 #ifdef _WIN32
 #include <malloc.h>
--- installer/installer.c
+++ installer/installer.c
@@ -12,7 +12,7 @@
 #include <unistd.h>
 #include <signal.h>
 
-#define VERSION_CODE "0.2.1.0001"
+#define VERSION_CODE "0.2.1.0005"
 
 #ifdef _WIN32
 #include <malloc.h>
