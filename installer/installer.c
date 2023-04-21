6044e60e51e11620ad060dfec87320cb512c6bee
Zhenrong WANG <wangzhenrong@hpc-now.com>
2023-04-21 13:47:52 +0800

    changed the caution info

--- installer/installer.c
+++ installer/installer.c
@@ -427,21 +427,21 @@ int install_services(int hpcopr_loc_flag, char* hpcopr_loc, int crypto_loc_flag,
 int uninstall_services(void){
     char doubleconfirm[128]="";
     // Double confirmation is needed.
-    printf("|*                                C A U T I O N !                                  *\n");
-    printf("|*                                                                                 *\n");
-    printf("|*   YOU ARE UNINSTALLING THE HPC-NOW SERVICES, PLEASE CONFIRM THE ISSUES BELOW:   *\n");
-    printf("|*                                                                                 *\n");
-    printf("|*   1. You have *DESTROYED* all the clusters managed by this device.              *\n");
-    printf("|*      This is * !!! EXTREMELY IMPORTANT !!! *                                    *\n");
-    printf("|*   2. You have *CHECKED* your cloud service account and all the resources        *\n");
-    printf("|*      created by the HPC-NOW services on this device have been destructed.       *\n");
-    printf("|*   3. You have *EXPORTED* the usage log and systemlog to a permenant directory,  *\n");
-    printf("|*      You can run 'hpcopr syslog' and 'hpcopr usage' to get the logs and save    *\n");
-    printf("|*      them to a directory such as /Users/ANOTHER_USER                            *\n");
-    printf("|*                                                                                 *\n");
-    printf("|*                       THIS OPERATION IS UNRECOVERABLE!                          *\n");
-    printf("|*                                                                                 *\n");
-    printf("|*                                C A U T I O N !                                  *\n");
+    printf("|*                                C A U T I O N !                                  \n");
+    printf("|*                                                                                 \n");
+    printf("|*   YOU ARE UNINSTALLING THE HPC-NOW SERVICES, PLEASE CONFIRM THE ISSUES BELOW:   \n");
+    printf("|*                                                                                 \n");
+    printf("|*   1. You have *DESTROYED* all the clusters managed by this device.              \n");
+    printf("|*      This is * !!! EXTREMELY IMPORTANT !!! *                                    \n");
+    printf("|*   2. You have *CHECKED* your cloud service account and all the resources        \n");
+    printf("|*      created by the HPC-NOW services on this device have been destructed.       \n");
+    printf("|*   3. You have *EXPORTED* the usage log and systemlog to a permenant directory,  \n");
+    printf("|*      You can run 'hpcopr syslog' and 'hpcopr usage' to get the logs and save    \n");
+    printf("|*      them to a directory such as /Users/ANOTHER_USER                            \n");
+    printf("|*                                                                                 \n");
+    printf("|*                       THIS OPERATION IS UNRECOVERABLE!                          \n");
+    printf("|*                                                                                 \n");
+    printf("|*                                C A U T I O N !                                  \n");
     printf("|  ARE YOU SURE? Only 'y-e-s' is accepted to double confirm this operation:\n");
     printf("[ INPUT: ] ");
     fflush(stdin);
@@ -495,12 +495,12 @@ int update_services(int hpcopr_loc_flag, char* hpcopr_loc, int crypto_loc_flag, 
         printf("[ FATAL: ] Exit now.\n");
         return 1;
     }
-    printf("|*                                C A U T I O N !                                  *\n");
-    printf("|*                                                                                 *\n");
-    printf("|*     YOU ARE UPDATING THE HPC-NOW SERVICES. THE CURRENT hpcopr BINARY WILL BE    *\n");
-    printf("|*     REPLACED. IF YOU UPDATE WITH THE hpcoprloc= and/or cryptoloc=, PLEASE MAKE  *\n");
-    printf("|*     SURE THE LOCATION(S) POINT(S) TO VALID EXECUTABLE(S).                       *\n");
-    printf("|*                                                                                 *\n");
+    printf("|*                                C A U T I O N !                                  \n");
+    printf("|*                                                                                 \n");
+    printf("|*     YOU ARE UPDATING THE HPC-NOW SERVICES. THE CURRENT hpcopr BINARY WILL BE    \n");
+    printf("|*     REPLACED. IF YOU UPDATE WITH THE hpcoprloc= and/or cryptoloc=, PLEASE MAKE  \n");
+    printf("|*     SURE THE LOCATION(S) POINT(S) TO VALID EXECUTABLE(S).                       \n");
+    printf("|*                                                                                 \n");
     printf("| ARE YOU SURE? Only 'y-e-s' is accepted to double confirm this operation:\n");
     printf("[ INPUT: ] ");
     fflush(stdin);
