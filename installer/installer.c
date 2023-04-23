974eed6204325ca0f50f0add60d8c85828f211c3
Zhenrong WANG <wangzhenrong@hpc-now.com>
2023-04-23 14:19:13 +0800

    restructure the whole code

--- installer/installer.c
+++ installer/installer.c
@@ -21,8 +21,7 @@
  *   reset_string (from general_funcs.c)
  *   fgetline (from general_funcs.c)
  *   generate_random_passwd (from general_funcs.c)
- * 
- * Considering combine the 2 codes
+ *
  */
 
 int check_internet_installer(void){
