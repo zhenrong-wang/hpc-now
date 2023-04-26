d819d9ba092e4db77ec94694963df9cbd4333735
zhenrongwang <zhenrongwang@live.com>
2023-04-25 21:30:59 -0700

    Minor version code changed

--- hpcopr/now_macros.h
+++ hpcopr/now_macros.h
@@ -9,7 +9,7 @@
 #ifndef NOW_MACROS_H
 #define NOW_MACROS_H
 
-#define CORE_VERSION_CODE "0.2.0.0043"
+#define CORE_VERSION_CODE "0.2.0.0044"
 
 #ifdef _WIN32
 #define CRYPTO_KEY_FILE "C:\\programdata\\hpc-now\\now_crypto_seed.lock" // This is a global file!
