40483e8b08fee9fe4d90a846e0569a289c5f6b28
zhenrongwang <zhenrongwang@live.com>
2023-05-06 15:22:45 +0800

    Revert the now-crypto dev.

--- now-crypto/now-crypto.c
+++ now-crypto/now-crypto.c
@@ -7,43 +7,63 @@
  */
 
 /*
- * This is a *psedo* and very *simple* symmetric crypto program! Not a real or strict one. If you'd like to use a real
- * crypto program, try AES, or at least DES. Maybe we will switch to one of them in the future.
+ * This is a *psedo* and very *simple* crypto program! Not a real one. If you'd like to use a real
+ * crypto program, try AES, or at least DES. Maybe we will switch to it in th future.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-#define CH_BUFFER_MAX 1048576
-                      
-int read_file(char* filename, char* ch_array){
-    int i=0;
-    int ch='\0';
-    FILE* file_p=fopen(filename,"r");
-    if(file_p==NULL){
-        return -1;
+int file_encryption(FILE* orig_file, FILE* target_file, int encrypt_key){
+    int real_key=((encrypt_key%1000*17+1301)%100+19)*7%100;
+    int salt_key=real_key%31;
+    int origc='\0';
+    int newc='\0';
+    int i=1,j=1;
+    if(orig_file==NULL||target_file==NULL ){
+        return 1;
     }
-    while((ch=fgetc(file_p))!=EOF){
-        *(ch_array+i)=ch;
+    origc=fgetc(orig_file);
+    while(origc!=EOF){
+        if(i%salt_key==0){
+            newc=origc+real_key+(i*j)%salt_key;
+            j++;
+        }
+        else{
+            newc=origc+real_key+i%salt_key;
+        }
         i++;
+        fputc(newc,target_file);
+        origc=fgetc(orig_file);
     }
-    *(ch_array+i)='\0';
-    fclose(file_p);
-    return i;
+    return 0;
 }
 
-int write_file(char* filename, char* ch_array, int file_length){
-    int i;
-    FILE* file_p=fopen(filename,"w+");
-    if(file_p==NULL){
-        return -1;
+int file_decryption(FILE* orig_file, FILE* target_file, int encrypt_key){
+    int real_key=((encrypt_key%1000*17+1301)%100+19)*7%100;
+    int salt_key=real_key%31;
+    int origc='\0';
+    int newc='\0';
+    int i=1,j=1;
+    if(orig_file==NULL||target_file==NULL ){
+        return 1;
     }
-    for(i=0;i<file_length;i++){
-        fputc(*(ch_array+i),file_p);
+    origc=fgetc(orig_file);
+    while(origc!=EOF){
+        if(i%salt_key==0){
+            newc=origc-real_key-(i*j)%salt_key;
+            j++;
+        }
+        else{
+            newc=origc-real_key-i%salt_key;
+        }
+        i++;
+        fputc(newc,target_file);
+        origc=fgetc(orig_file);
     }
-    fclose(file_p);
-    return 0;
+  return 0;
 }
 
 int md5convert(char* md5string){
@@ -55,73 +75,15 @@ int md5convert(char* md5string){
     for(i=0;i<length;i++){
         sum+=*(md5string+i);
     }
-    sum=(sum*13+17)*37+41;
+    sum=((sum*13+17)*19+31)*37+41;
     return sum;
-}
-
-int file_encryption_decryption(char* option, char* orig_file, char* target_file, int encrypt_key){
-    if(strcmp(option,"encrypt")!=0&&strcmp(option,"decrypt")!=0){
-        return 1;
-    }
-    int real_key=((encrypt_key%1000*17+1301)%100+19)*7%31;
-    int salt_position=real_key%29+2;
-    int i=0,j=0,file_length;
-    char ch_array_buffer[CH_BUFFER_MAX]="";
-    FILE* filep_orig=fopen(orig_file,"r");
-    if(filep_orig==NULL){
-        return -1;
-    }
-    FILE* filep_target=fopen(target_file,"w+");
-    if(filep_target==NULL){
-        fclose(filep_orig);
-        return -1;
-    }
-    fclose(filep_target);
-    file_length=read_file(orig_file,ch_array_buffer);
-//    printf("%d,%d,%d,%d\n",file_length,encrypt_key,salt_position,real_key);
-    do{
-        if(i%salt_position==0){
-            if(strcmp(option,"encrypt")==0){
-                *(ch_array_buffer+i)+=((i*j)%7+real_key);
-            }
-            else{
-                *(ch_array_buffer+i)-=((i*j)%7+real_key);
-            }
-            i++;
-            j++;
-            continue;
-        }
-        else if(i%2==0){
-            if(strcmp(option,"encrypt")==0){
-                *(ch_array_buffer+i)+=real_key;
-            }
-            else{
-                *(ch_array_buffer+i)-=real_key;
-            }
-            i++;
-            continue;
-        }
-        else{
-            if(strcmp(option,"encrypt")==0){
-                *(ch_array_buffer+i)+=(i%7+real_key);
-            }
-            else{
-                *(ch_array_buffer+i)-=(i%7+real_key);
-            }
-            i++;
-            continue;
-        }
-    }while(i<file_length);
-    fclose(filep_orig);
-    write_file(target_file,ch_array_buffer,file_length);
-    return 0;
 }
 
 int main(int argc,char *argv[]){
     FILE* orig_file=NULL;
     FILE* target_file=NULL;
     if(argc!=5){
-        printf("Error: Wrong Parameter Numbers.\n"); 
+        printf("Error: Not Enough parameters.\n"); 
         return -1;
     }
     if(strcmp(argv[1],"encrypt")!=0&&strcmp(argv[1],"decrypt")!=0){
@@ -137,12 +99,18 @@ int main(int argc,char *argv[]){
         printf("Error: Cannot open original file.\n");
         return -5;
     }
-    fclose(orig_file);
     target_file=fopen(argv[3],"w+");
     if(target_file==NULL){
         printf("Error: Cannot create target file.\n");
         return -5;
     }
+    if(strcmp(argv[1],"encrypt")==0){
+        file_encryption(orig_file,target_file,md5convert(argv[4]));
+    }
+    else{
+        file_decryption(orig_file,target_file,md5convert(argv[4]));
+    }
+    fclose(orig_file);
     fclose(target_file);
-    return file_encryption_decryption(argv[1],argv[2],argv[3],md5convert(argv[4]));
+    return 0; 
 }
