e7a575e4513b56ebf0020e2042c3dba7155f44a5
zhenrongwang <zhenrongwang@live.com>
2023-05-30 09:00:59 +0800

    Fixed a minor bug in viewlog command.

--- hpcopr/hpcopr_main.c
+++ hpcopr/hpcopr_main.c
@@ -871,13 +871,13 @@ int main(int argc, char* argv[]){
     }
 
     if(strcmp(argv[1],"viewlog")==0){
-        if(argc==2){
+        if(argc==2||(argc==3&&command_flag==2)){
             run_flag=view_run_log(workdir,"","","");
         }
-        else if(argc==3){
+        else if(argc==3||(argc==4&&command_flag==2)){
             run_flag=view_run_log(workdir,argv[2],"","");
         }
-        else if(argc==4){
+        else if(argc==4||(argc==5&&command_flag==2)){
             run_flag=view_run_log(workdir,argv[2],argv[3],"");
         }
         else{
