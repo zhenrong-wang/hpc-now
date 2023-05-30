ce420fa31ea48555bcf42e743c4860f280a8d016
zhenrongwang <zhenrongwang@live.com>
2023-05-30 17:26:51 +0800

    Updated the installer

--- installer/installer.h
+++ installer/installer.h
@@ -9,7 +9,7 @@
 #ifndef INSTALLER_H
 #define INSTALLER_H
 
-#define INSTALLER_VERSION_CODE "0.2.0.0117"
+#define INSTALLER_VERSION_CODE "0.2.0.0119"
 #define DEFAULT_URL_HPCOPR_LATEST "https://hpc-now-1308065454.cos.ap-guangzhou.myqcloud.com/hpcopr-0.2.1/"
 
 int check_internet_installer(void);
@@ -18,10 +18,12 @@ void print_tail_installer(void);
 void print_help_installer(void);
 int check_current_user_root(void);
 int license_confirmation(void);
-int install_services(int hpcopr_loc_flag, char* hpcopr_loc, int crypto_loc_flag, char* now_crypto_loc);
+int install_services(int hpcopr_loc_flag, char* hpcopr_loc, char* hpcopr_ver, int crypto_loc_flag, char* now_crypto_loc);
 int uninstall_services(void);
-int update_services(int hpcopr_loc_flag, char* hpcopr_loc, int crypto_loc_flag, char* now_crypto_loc);
+int update_services(int hpcopr_loc_flag, char* hpcopr_loc, char* hpcopr_ver, int crypto_loc_flag, char* now_crypto_loc);
 int valid_loc_format_or_not(char* loc_string);
 int split_parameter(char* param, char* param_head, char* param_tail);
+int get_valid_verlist(void);
+int version_valid(char* hpcopr_ver);
 
 #endif
