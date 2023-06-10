6d128c3842401e8cf8ca88f12ba459e6c5533ed6
zhenrongwang <zhenrongwang@live.com>
2023-06-10 14:19:08 +0800

    Added a header.

--- hpcopr/prereq_check.c
+++ hpcopr/prereq_check.c
@@ -10,7 +10,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <unistd.h>
 
 #include "now_macros.h"
 #include "general_funcs.h"
--- installer/installer.c
+++ installer/installer.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #ifdef _WIN32
 #include "..\\hpcopr\\now_macros.h"
