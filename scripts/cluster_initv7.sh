0d9506b07fe7e71c01e7dbe7c929fee3f5937699
zhenrongwang <zhenrongwang@live.com>
2023-07-05 14:44:10 +0800

    Tuned the sudo permission to user1

--- scripts/cluster_initv7.sh
+++ scripts/cluster_initv7.sh
@@ -125,7 +125,9 @@ if [ -f /root/hostfile ]; then
 fi
 
 ############ Add Users ####################
-echo -e "user1 ALL=(ALL) NOPASSWD:SETENV: ALL" >> /etc/sudoers
+echo -e "user1 ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sed -i 's/env_reset/!env_reset/g' /etc/sudoers /etc/sudoers
+sed -i 's/Defaults    secure_path/#Defaults    secure_path/g' /etc/sudoers # Granted sudo permissions to user1
 chmod 511 /usr/bin/passwd # Disable ordinary users to change its own password
 while read user_row
 do
