00ba0f089e47a00489e52369f6e1cf743e38278a
zhenrongwang <zhenrongwang@live.com>
2023-08-16 22:32:47 +0800

    Updated the init script

--- scripts/cluster_initv7.sh
+++ scripts/cluster_initv7.sh
@@ -112,7 +112,7 @@ if [ -f /root/hostfile ]; then
   mkdir -p /hpc_data/cluster_data
   chmod -R 644 /hpc_data/cluster_data
   chmod 755 /hpc_data/cluster_data
-  if [ cloud_flag = 'CLOUD_E' ]; then
+  if [ $cloud_flag = 'CLOUD_E' ]; then
     mkdir -p /hpc_data/cluster_data/.bucket_creds
     chmod -R 644 /hpc_data/cluster_data/.bucket_creds
   fi
