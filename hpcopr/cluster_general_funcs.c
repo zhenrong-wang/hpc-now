7253d2f6dde7d980be41e675639e4223900f597c
zhenrongwang <zhenrongwang@live.com>
2023-12-04 12:17:38 +0800

    Minor change.

--- hpcopr/cluster_general_funcs.c
+++ hpcopr/cluster_general_funcs.c
@@ -1445,7 +1445,7 @@ int tf_execution(tf_exec_config* tf_run, char* execution_name, char* workdir, ch
     }
     system(cmdline);
     if(silent_flag!=0){
-        printf(WARN_YELLO_BOLD "[ -WARN- ] Do not terminate this process. TF: %s. Max Exec Time: %d s.\n",tf_run->tf_runner_type,tf_run->max_wait_time);
+        printf(WARN_YELLO_BOLD "[ -WARN- ] Do not terminate this process. TF: %s. Max Exec Time: %ds.\n",tf_run->tf_runner_type,tf_run->max_wait_time);
         printf("|          Command: %s. Debug Level: %s. Log: " RESET_DISPLAY HIGH_GREEN_BOLD "hpcopr -b viewlog\n" RESET_DISPLAY,execution_name,tf_run->dbg_level);
     }
     if(wait_for_complete(tf_realtime_log,execution_name,tf_run->max_wait_time,tf_error_log,tf_error_log_archive,1)!=0){
