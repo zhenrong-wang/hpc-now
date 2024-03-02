d67f8558e5c49811198607367e92657eaa419d79
zhenrong-wang <zhenrongwang@live.com>
2024-03-02 11:41:34 +0800

    Updated the transfer module.

--- hpcopr/transfer.c
+++ hpcopr/transfer.c
@@ -68,13 +68,17 @@ int get_import_info(char cluster_name_output[], unsigned int name_len_max, char 
     return 1;
 }
 
-int user_list_check(char* cluster_name, char* user_list_read, char* user_list_final, int* user1_flag){
+char* user_list_check(char* cluster_name, char* user_list_read, int* user1_flag){
     int i=1;
     int j;
     int k=0;
     int valid_user_num=0;
-    char user_list_buffer[1024]="";
     char username_temp[32]="";
+    char* user_list_buffer=(char*)malloc(strlen(user_list_read)+1);
+    if(user_list_buffer==NULL){
+        return NULL;
+    }
+    memset(user_list_buffer,'\0',strlen(user_list_read)+1);
     do{
         get_seq_nstring(user_list_read,':',i,username_temp,32);
         if(strcmp(username_temp,"user1")==0){
@@ -83,6 +87,9 @@ int user_list_check(char* cluster_name, char* user_list_read, char* user_list_fi
         i++;
         if(strcmp(username_temp,"root")==0){
             continue; /* root user cannot be exported. */
+        }
+        if(k==strlen(user_list_read)){
+            return user_list_buffer;
         }
         if(user_name_quick_check(cluster_name,username_temp,SSHKEY_DIR)==0){
             valid_user_num++;
@@ -94,9 +101,8 @@ int user_list_check(char* cluster_name, char* user_list_read, char* user_list_fi
             k++;
         }
     }while(strlen(username_temp)!=0);
-    *(user_list_buffer+k)='\0';
-    strcpy(user_list_final,user_list_buffer);
-    return valid_user_num;
+
+    return user_list_buffer;
 }
 
 int export_cluster(char* cluster_name, char* user_list, char* admin_flag, char* crypto_keyfile, char* password, char* export_target_file, int batch_flag_local){
@@ -127,7 +133,7 @@ int export_cluster(char* cluster_name, char* user_list, char* admin_flag, char* 
     char real_export_folder[FILENAME_LENGTH_EXT]="";
     char export_filename[1024]="";
     char user_line_buffer[LINE_LENGTH_SHORT]="";
-    char real_user_list[1024]="";
+    char* real_user_list=NULL;
     char hash_key_current[64]="";
     char hash_key_trans[64]="";
     char user_list_buffer[1024]="";
@@ -170,17 +176,17 @@ int export_cluster(char* cluster_name, char* user_list, char* admin_flag, char* 
         }
     }
     else{
-        strcpy(user_list_buffer,user_list);
+        strncpy(user_list_buffer,user_list,1023);
     }
     if(strcmp(user_list_buffer,"ALL")==0||strcmp(user_list_buffer,"all")==0||strcmp(user_list_buffer,"All")==0){
         printf(WARN_YELLO_BOLD "[ -WARN- ] Exporting *ALL* users of cluster %s . NOT RECOMMENDED!\n" RESET_DISPLAY,cluster_name);
-        strcpy(real_user_list,"all");
+        real_user_list=(char*)"all";
         strcpy(real_admin_flag,"admin");
         user1_flag=1;
     }
     else{
-        export_user_num=user_list_check(cluster_name,user_list_buffer,real_user_list,&user1_flag);
-        if(export_user_num==0){
+        real_user_list=user_list_check(cluster_name,user_list_buffer,&user1_flag);
+        if(real_user_list==NULL){
             printf(FATAL_RED_BOLD "[ FATAL: ] The specified user list is invalid." RESET_DISPLAY "\n");
             return -1;
         }
@@ -196,21 +202,26 @@ int export_cluster(char* cluster_name, char* user_list, char* admin_flag, char* 
     if(strlen(password)==0){
         if(batch_flag_local==0){
             printf(FATAL_RED_BOLD "[ FATAL: ] Password not specified. Use -p PASSWORD ." RESET_DISPLAY "\n");
+            free(real_user_list);
             return 17;
         }
-        printf(GENERAL_BOLD "[ -INFO- ]" RESET_DISPLAY " Please input a *complex* password to encrypt and export.\n");
+        printf(GENERAL_BOLD "[ -INFO- ]" RESET_DISPLAY " Set a password with 3 of 4 types: " GENERAL_BOLD "a-z, A-Z, 0-9, %s" RESET_DISPLAY "\n",SPECIAL_PASSWORD_CHARS);
         getpass_stdin("[ INPUT: ] Type a password: ",real_password,128);
-        /*password_temp=GETPASS_FUNC("[ INPUT: ] *without echo* ");
-        strcpy(real_password,password_temp);*/
     }
     else{
         strcpy(real_password,password);
+    }
+    if(password_complexity_check(real_password,SPECIAL_PASSWORD_CHARS)!=0){
+        printf(WARN_YELLO_BOLD "[ -WARN- ] The provided password " RESET_DISPLAY GENERAL_BOLD "%s" RESET_DISPLAY WARN_YELLO_BOLD " is not complex enough." RESET_DISPLAY "\n",real_password);
+        generate_random_npasswd(real_password,17,SPECIAL_PASSWORD_CHARS,strlen(SPECIAL_PASSWORD_CHARS));
+        printf(WARN_YELLO_BOLD "[ -WARN- ] Automatic generated: " RESET_DISPLAY GENERAL_BOLD "%s" RESET_DISPLAY "\n",real_password);
     }
     password_sha_hash(real_password,hash_key_trans,64);
     local_path_nparser(export_target_file,filename_temp,FILENAME_LENGTH);
     if(strlen(filename_temp)==0){
         if(batch_flag_local==0){
             printf(FATAL_RED_BOLD "[ FATAL: ] Export path not specified. Use -d EXPORT_DEST." RESET_DISPLAY "\n");
+            free(real_user_list);
             return 17;
         }
         printf(GENERAL_BOLD "[ -INFO- ]" RESET_DISPLAY " Please input a path (folder or file) to export." RESET_DISPLAY "\n");
@@ -246,18 +257,22 @@ int export_cluster(char* cluster_name, char* user_list, char* admin_flag, char* 
     printf(GENERAL_BOLD "[ -INFO- ]" RESET_DISPLAY " Creating temperory directories ...\n");
     rm_file_or_dir(tmp_root);
     if(mk_pdir(tmp_root)<0){
+        free(real_user_list);
         return -5;
     }
     snprintf(dirname_temp,DIR_LENGTH_EXT+15,"%s%s%s%slog",tmp_root,PATH_SLASH,cluster_name,PATH_SLASH);
     if(mk_pdir(dirname_temp)<0){
+        free(real_user_list);
         return -5;
     }
     snprintf(dirname_temp,DIR_LENGTH_EXT+15,"%s%s%s%sconf",tmp_root,PATH_SLASH,cluster_name,PATH_SLASH);
     if(mk_pdir(dirname_temp)<0){
+        free(real_user_list);
         return -5;
     }
     snprintf(dirname_temp,DIR_LENGTH_EXT+15,"%s%s.terraform",tmp_stackdir,PATH_SLASH);
     if(mk_pdir(dirname_temp)<0||mk_pdir(tmp_vaultdir)<0||mk_pdir(tmp_sshdir)<0){
+        free(real_user_list);
         return -5;
     }
     snprintf(cluster_name_flag,FILENAME_LENGTH-1,"%s%scluster_name_flag.txt",HPC_NOW_ROOT_DIR,PATH_SLASH);
@@ -266,6 +281,7 @@ int export_cluster(char* cluster_name, char* user_list, char* admin_flag, char* 
     if(file_p==NULL){
         printf(FATAL_RED_BOLD "[ FATAL: ] Failed to create cluster name flag file." RESET_DISPLAY "\n");
         rm_file_or_dir(tmp_root);
+        free(real_user_list);
         return -5;
     }
     fprintf(file_p,"%s\ncluster_name: %s\nunique_id: %s\n",TRANSFER_HEADER,cluster_name,unique_id);
@@ -273,6 +289,7 @@ int export_cluster(char* cluster_name, char* user_list, char* admin_flag, char* 
     printf(GENERAL_BOLD "[ -INFO- ]" RESET_DISPLAY " Exporting related files ...\n");
     if(get_file_sha_hash(crypto_keyfile,hash_key_current,64)!=0){
         rm_file_or_dir(tmp_root);
+        free(real_user_list);
         return -5;
     }
     snprintf(source_file,FILENAME_LENGTH-1,"%s%scurrentstate.tmp",current_stackdir,PATH_SLASH);
@@ -371,6 +388,7 @@ next_user:
         printf(FATAL_RED_BOLD "[ FATAL: ] Failed to create cluster state flag file." RESET_DISPLAY "\n");
         rm_file_or_dir(tmp_root);
         delete_decrypted_files(workdir,crypto_keyfile);
+        free(real_user_list);
         return -5;
     }
     fprintf(file_p,"%s\nThis is not a genuine terraform-related file. Only for users of cluster %s. DO NOT DELETE THIS FILE.\n",INTERNAL_FILE_HEADER,cluster_name);
@@ -413,6 +431,7 @@ next_user:
     rm_file_or_dir(cluster_name_flag_tmp);
     if(file_exist_or_not(export_filename)!=0){
         printf(FATAL_RED_BOLD "[ FATAL: ] Failed to export the cluster " RESET_DISPLAY WARN_YELLO_BOLD "%s" RESET_DISPLAY FATAL_RED_BOLD " .\n" RESET_DISPLAY,cluster_name);
+        free(real_user_list);
         return 1;
     }
     else{
@@ -426,6 +445,7 @@ next_user:
         else{
             printf(GENERAL_BOLD "[  ****  ] +- Admin Privilege" RESET_DISPLAY ": " HIGH_CYAN_BOLD "NO" RESET_DISPLAY "\n");
         }
+        free(real_user_list);
         return 0;
     }
 }
--- hpcopr/transfer.h
+++ hpcopr/transfer.h
@@ -9,7 +9,7 @@
 #define TRANSFER_H
 
 int get_import_info(char cluster_name_output[], unsigned int name_len_max, char tmp_top_output[], unsigned int dir_len_max, char unique_id[], unsigned int id_len_max, char* tmp_import_root, char* md5sum);
-int user_list_check(char* cluster_name, char* user_list_read, char* user_list_final, int* user1_flag);
+char* user_list_check(char* cluster_name, char* user_list_read, int* user1_flag);
 int export_cluster(char* cluster_name, char* user_list, char* admin_flag, char* crypto_keyfile, char* password, char* export_target_file, int batch_flag_local);
 int import_cluster(char* zip_file, char* password, char* crypto_keyfile, int batch_flag_local);
 int update_cluster_status(char* cluster_name, char* currentstate);
