e426af0250125464a6c4054ac5ecadc2b7c82329
Zhenrong Wang <zhenrongwang@live.com>
2024-03-13 15:55:04 +0800

    Minor changed the windows build script

--- make_windows.bat
+++ make_windows.bat
@@ -19,7 +19,7 @@ if "%~1"=="" (
     echo [ -INFO- ] Please build hpcmgr with GNU/Linux, not Windows.
 	mkdir .\build > nul 2>&1
 	echo [ -INFO- ] Deleting previously built binaries ^(if exist^)...
-	del /s /q /f .\build\*
+	del /s /q /f .\build\* > nul
 	echo [ -INFO- ] Bulding new binaries with the gcc ...
 	gcc .\hpcopr\*.c -Wall -lpthread -o .\build\hpcopr-win-%hpcopr_version_code%.exe
 	gcc -c .\hpcopr\general_funcs.c -Wall -o .\installer\gfuncs.o
@@ -32,16 +32,16 @@ if "%~1"=="" (
     ar -rc .\installer\libnow.a .\installer\gfuncs.o .\installer\ocrypto.o .\installer\cgfuncs.o .\installer\tproc.o .\installer\md5.o .\installer\gprint.o .\installer\sha256.o
 	gcc .\installer\installer.c .\installer\libnow.a -lnetapi32 -lpthread -Wall -o .\build\installer-win-%installer_version_code%.exe
 	gcc .\now-crypto\now-crypto-v3-aes.c -Wall -Ofast -o .\build\now-crypto-aes-win.exe
-	del /f /s /q .\installer\*.a
-	del /f /s /q .\installer\*.o	
+	del /f /s /q .\installer\*.a > nul
+	del /f /s /q .\installer\*.o > nul
 ) else if "%~1"=="delete" (
 	echo [ START: ] Deleting the binaries now ...
-	del /s /q /f .\build\*
+	del /s /q /f .\build\* > nul
 ) else if "%~1"=="clear" (
 	echo [ START: ] Deleting the binaries now ...
-	del /s /q /f .\build\*
+	del /s /q /f .\build\* > nul
 	echo [ START: ] Removing the build folder now ...
-	rd /s /q .\build
+	rd /s /q .\build > nul
 ) else (
 	echo [ -INFO- ] Please specify an option: 'build', 'delete', or 'clear'
 	echo ^|          build  - ^(re^)build the binaries
