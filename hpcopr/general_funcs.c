fd303e91ff31e8c0e06d20ef5959e2477370ed2a
zhenrong-wang <zhenrongwang@live.com>
2024-03-29 18:42:59 +0800

    Tuned for Windows pthread

--- hpcopr/general_funcs.c
+++ hpcopr/general_funcs.c
@@ -3718,6 +3718,7 @@ void* thread_getaddrinfo(void* arg){
         freeaddrinfo(server_info);
     }
     pthread_exit(NULL);
+    return NULL;
 }
 
 void close_socket(int socket_fd){
@@ -3811,7 +3812,7 @@ int check_connectivity(const char* domain, const char* port, const unsigned int 
             break;
         }
         if((thread_timer_nsec%500000000)==0){
-            printf("[ -INFO- ] Checking internet connectivity: " GREY_LIGHT "%.1lf" RESET_DISPLAY " / %ld sec ...\r",(double)thread_timer_nsec/1000000000,max_wait_sec);
+            printf("[ -INFO- ] Checking internet connectivity: " GREY_LIGHT "%.1lf" RESET_DISPLAY " / %d sec ...\r",(double)thread_timer_nsec/1000000000,max_wait_sec);
             fflush(stdout);
         }
         thread_timer_nsec+=sleep_time.tv_nsec;
